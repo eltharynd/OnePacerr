6262609c056cc78438092123375be2670dcca9c7
Laurent Schwitter <laurent.schwitter@k-lab.ch>
2026-06-17 15:06:09 +0200

    cleanup

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
-const decodeKey = (key: string) => {
-	if (!key) return null
-	if (/BEGIN/g.test(key)) return key
-	let decoded = Buffer.from(key, 'base64').toString()
-	if (/BEGIN/g.test(decoded)) return decoded
-}
-
 export default {
 	LOG_OUTPUT: process.env.LOG_OUTPUT || 'text',
 	LOCAL_DEV: /true/i.test(process.env.LOCAL_DEV || 'false'),
@@ -18,21 +11,4 @@ export default {
 	DOMAIN: process.env.DOMAIN || 'localhost',
 	API_BASE: process.env.API_BASE || '/api/v1/',
 	PORT: Number.parseInt(process.env.PORT || '3000'),
-
-	IGNORE_JWT_EXPIRATION: /true/i.test(
-		process.env.IGNORE_JWT_EXPIRATION || 'false',
-	),
-
-	AUTO_EXPIRATION_INTERVAL:
-		Number.parseInt(process.env.AUTO_EXPIRATION_INTERVAL || '1800') * 1000,
-
-	PRIVATE_KEY: decodeKey(process.env.PRIVATE_KEY),
-	PUBLIC_KEY: decodeKey(process.env.PUBLIC_KEY),
-
-	KC_BASE_URL:
-		process.env.KC_BASE_URL || 'http://keycloak.iam.svc.cluster.local:8080',
-	//process.env.KC_BASE_URL || 'http://127.0.0.1:8080',
-
-	KC_BOOTSTRAP_ADMIN_PASSWORD:
-		process.env.KC_BOOTSTRAP_ADMIN_PASSWORD || 'password',
 }
--- src/util/context.ts
+++ src/util/context.ts
@@ -1,14 +1,11 @@
 import { Express } from '../api/express.js'
-import { KeycloakManager } from '../keycloak/keycloak.manager.js'
 
 class ContextContainer {
 	express: Express
-	keycloak: KeycloakManager
 }
 
 export const Context = new ContextContainer()
 
 export default {
 	express: Express,
-	keycloak: KeycloakManager,
 }
