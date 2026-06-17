a9f6e559efe846d78889a6c46fdf09ea56eb0ef9
Laurent Schwitter <laurent.schwitter@k-lab.ch>
2026-06-17 15:08:15 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
 import 'reflect-metadata'
 import { Express } from './api/express.js'
-import environment from './environment.js'
 import { Context } from './util/context.js'
-import { KeycloakManager } from './keycloak/keycloak.manager.js'
+
 import Logger from './util/logger.js'
 
 const startApp = async () => {
-	let expirationInterval
-
-	if (!environment.PRIVATE_KEY || !environment.PUBLIC_KEY) {
-		Logger.error(
-			`Couldn't load RSA key pair... Make sure you setup the project correctly...`,
-		)
-		return process.exit(-1)
-	}
-
 	let gracefulClose = async () => {
 		try {
 			Logger.info('GRACEFULLY QUITTING APPLICATION...')
 
-			//CLOSE COMPONENTS GRACEFULLY HERE
-			if (expirationInterval) {
-				clearInterval(expirationInterval)
-				expirationInterval = null
-			}
+			//GRACEFUL QUIT HERE
 
 			Logger.info('GRACEFULLY CLOSED APPLICATION...')
 			process.exit(0)
@@ -43,23 +29,6 @@ const startApp = async () => {
 		await Context.express.start()
 
 		Logger.info('APPLICATION STARTED SUCCESSFULLY...')
-
-		Logger.info('TRYING INITIALIZE KEYCLOAK...')
-		try {
-			Context.keycloak = new KeycloakManager()
-			await Context.keycloak.initialize()
-			expirationInterval = setInterval(async () => {
-				try {
-					await Context.keycloak.processExpirations()
-				} catch (e) {
-					Logger.error('FAILED TO EXPIRE USERS...')
-					throw e
-				}
-			}, environment.AUTO_EXPIRATION_INTERVAL)
-		} catch (e) {
-			Logger.error('FAILED TO INITIALIZE KEYCLOAK...')
-			if (!environment.LOCAL_DEV) throw e
-		}
 	} catch (e) {
 		Logger.error('APPLICATION COULD NOT BE STARTED...')
 		Logger.error(e)
