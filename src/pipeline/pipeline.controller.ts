ef4ec60b689e6e864eb6c3b69f6119577153d7fe
Laurent Schwitter <laurent.schwitter@gmail.com>
2026-06-25 23:12:01 +0200

    monitored count in pipeline running meessage [skip ci]

--- src/pipeline/pipeline.controller.ts
+++ src/pipeline/pipeline.controller.ts
@@ -107,6 +107,9 @@ export class PipelineController {
 		Logger.info(`##################################`)
 		Logger.info(`####                          ####`)
 		Logger.info(`####     PIPELINE RUNNING     ####`)
+		Logger.info(
+			`####   (monitored ep: ${String(this.report.monitored.length).padEnd(3, ' ')} )   ####`,
+		)
 		Logger.info(`####                          ####`)
 		Logger.info(`##################################`)
 		Logger.info(`##################################`)
